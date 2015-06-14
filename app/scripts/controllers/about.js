1d0a75135f22239423a300c9931af9bd4d791f75
Manuel Dupont <Mano@Macintosh.local>
2015-06-14 18:31:21 +0200

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crfRdpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the crfRdpApp
+ */
+angular.module('crfRdpApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crfRdpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the crfRdpApp
+ */
+angular.module('crfRdpApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
